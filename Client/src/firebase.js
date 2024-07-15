053270ebb5dfb9c8def191daee53e91617b83432
Piyush Gaur <121219215+piyushgr@users.noreply.github.com>
2024-07-16 00:44:25 +0530

    fixed bugs

--- Client/src/firebase.js
+++ Client/src/firebase.js
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: "AIzaSyARHUkUG3GnAcHLV4DjZ9cqS56fPElH7TE",
   authDomain: "blogapp-fae5b.firebaseapp.com",
   projectId: "blogapp-fae5b",
   storageBucket: "blogapp-fae5b.appspot.com",
